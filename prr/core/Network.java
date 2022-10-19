0069fc205c638f96d20195483adbb16ec467ff74
Diogo Correia <diogo.filipe.correia@tecnico.ulisboa.pt>
2022-10-19 20:17:37 +0100

    Comentarios feitos.

--- prr/core/Network.java
+++ prr/core/Network.java
@@ -16,6 +16,9 @@ import prr.core.exception.UnrecognizedEntryException;
 
 /**
  * Class Store implements a store.
+ * Class that implements all the functionalities of the App.
+ * Stores all the relevant Objects and implements methods on them.
+ * Can be Serializable to save it's current state and load it on a different session.
  */
 public class Network implements Serializable {
 
@@ -25,10 +28,20 @@ public class Network implements Serializable {
   /** Clients of the Network. */
   private Map<String, Client> _clients = new TreeMap<>();
 
-  /**Terminals of the Network. */
+  /** Terminals of the Network. */
   private Map<String, Terminal> _terminals = new TreeMap<>();
 
 
+  /**
+   * Register a Client in the Network.
+   * Receives all the attributes needed to register a Client, verifies them and
+   * creates a new Client in the Network.
+   * @param key Key of the Client.
+   * @param name Name of the Client.
+   * @param nif TaxNumber of the Client.
+   * @throws DuplicateClientException Exception thrown when the Client's Key is
+   * already registered in the Network.
+   */
   public void registerClient(String key, String name, int nif) throws DuplicateClientException {
     if (_clients.containsKey(key))
       throw new DuplicateClientException(key);
@@ -36,17 +49,41 @@ public class Network implements Serializable {
     _clients.put(key, client);
   }
 
+  /**
+   * Get a Client from it's Key.
+   * @param key Key of the Client.
+   * @return the Client associated with the given Key.
+   * @throws UnknownClientException Exception thrown when the given
+   * Key isn't registered in the Network.
+   */
   public Client showClient(String key) throws UnknownClientException {
     if (!_clients.containsKey(key))
       throw new UnknownClientException(key);
     return _clients.get(key);
   }
 
+  /**
+   * Get a list of all the Client's in the Network.
+   * @return a list of all the Client's in the Network.
+   */
   public List<Client> showAllClients() {
     List<Client> temp = new ArrayList<>(_clients.values());
     return temp;
   }
 
+  /**
+   * Register a Terminal in the Network.
+   * Receives all the attributes needed to register a Terminal, verifies them and
+   * creates a new Terminal in the Network.
+   * @param id Key of the Terminal.
+   * @param type Type of the Terminal.
+   * @param clientKey Key of the Owner of the Terminal.
+   * @return the instance of the registered Terminal
+   * @throws DuplicateTerminalException Exception thrown when the Terminal's Key is
+   * already registered in the Network.
+   * @throws InvalidTerminalException Exception thrown when Terminal's doesn't match the specified format.
+   * @throws UnknownClientException Exception thrown when the Owner's Key isn't registered in the Network.
+   */
   public Terminal registerTerminal(String id, String type, String clientKey) throws DuplicateTerminalException, InvalidTerminalException, UnknownClientException{
     if (_terminals.containsKey(id))
       throw new DuplicateTerminalException(id);
@@ -72,17 +109,37 @@ public class Network implements Serializable {
     return null;
   }
 
+  /**
+   * Get a Terminal from it's Key.
+   * @param id Key of the Terminal.
+   * @return the Terminal associated with the given Key.
+   * @throws UnknownTerminalException Exception thrown when the given Key isn't
+   * registered in the Network.
+   */
   public Terminal getTerminal(String id) throws UnknownTerminalException{
     if (!_terminals.containsKey(id))
       throw new UnknownTerminalException(id);
     return _terminals.get(id);
   }
 
+  /**
+   * Get a list of all the Terminals in the Network.
+   * @return a list of all the Terminals in the Network.
+   */
   public List<Terminal> showAllTerminals() {
     List<Terminal> temp = new ArrayList<>(_terminals.values());
     return temp;
   }
 
+  /**
+   * Add a friend to a specified Terminal.
+   * Checks if the to be added friend's Key is registered in the Network. If
+   * it is, adds said terminal to the friends list.
+   * @param terminal Key of the Terminal on which the friend is getting added.
+   * @param friend Key of the Terminal that is getting added as a friend.
+   * @throws UnknownTerminalException Exception thrown when the Key for the terminal
+   * to be added as a friend isn't registered in the Network.
+   */
   public void addFriend(String terminal, String friend) throws UnknownTerminalException{
     if (!_terminals.containsKey(friend))
       throw new UnknownTerminalException(friend);
@@ -90,6 +147,16 @@ public class Network implements Serializable {
     _terminals.get(terminal).addTerminalFriend(_terminals.get(friend));
   }
 
+  /**
+   * Remove a friend from a specified Terminal.
+   * Checks if the to be removed friend's Key is registered in
+   * the Network. If it is, removes it from the specified Terminal's
+   * friends list.
+   * @param terminal Key of the Terminal from which the friend is getting removed.
+   * @param friend Key of the Terminal that is getting removed as a friend.
+   * @throws UnknownTerminalException Exception thrown when the Key for the terminal
+   * to be removed as a friend isn't registered in the Network.
+   */
   public void removeFriend(String terminal, String friend) throws UnknownTerminalException{
     if (!_terminals.containsKey(friend))
       throw new UnknownTerminalException(friend);
@@ -97,6 +164,12 @@ public class Network implements Serializable {
     _terminals.get(terminal).removeTerminalFriend(friend);
   }
 
+  /**
+   * Iterate through all the Terminals in the Network and check if
+   * they have done any past Communications. If they haven't then
+   * they get added to the Array to be returned.
+   * @return an Array of all the Terminals with no past Communications.
+   */
   public List<Terminal> getNoActivityTerminals() {
     List<Terminal> list = new ArrayList<>();
     for (Terminal t: _terminals.values())
