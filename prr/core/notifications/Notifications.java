518c4a735312185323989a1e22a2ad61cec8335e
Joao Santos <j.pedro.santos@tecnico.ulisboa.pt>
2022-10-30 16:04:04 +0000

    Notifications

---
+++ prr/core/notifications/Notifications.java
@@ -0,0 +1,23 @@
+package prr.core.notifications;
+
+import prr.core.terminal.Terminal;
+
+public class Notifications {
+
+    private final NotificationType _type;
+
+    private final Terminal _terminal;
+
+    public Notifications(NotificationType type, Terminal terminal) {
+        _type = type;
+        _terminal = terminal;
+    }
+
+    public NotificationType getType() {
+        return _type;
+    }
+
+    public String toString() {
+        return "" + _type + "|" + _terminal.getId();
+    }
+}
