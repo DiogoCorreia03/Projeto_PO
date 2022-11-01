bdb085d466f7c51ba6fcb570c7505f1d6d42e53e
Diogo Correia <diogo.filipe.correia@tecnico.ulisboa.pt>
2022-11-01 18:29:33 +0000

    Template para notifs

--- prr/core/notifications/Notification.java
+++ prr/core/notifications/Notification.java
@@ -2,13 +2,13 @@ package prr.core.notifications;
 
 import prr.core.terminal.Terminal;
 
-public class Notifications {
+public abstract class Notification {
 
     private final NotificationType _type;
 
     private final Terminal _terminal;
 
-    public Notifications(NotificationType type, Terminal terminal) {
+    public Notification(NotificationType type, Terminal terminal) {
         _type = type;
         _terminal = terminal;
     }
@@ -17,7 +17,7 @@ public class Notifications {
         return _type;
     }
 
-    public String toString() {
-        return _type + "|" + _terminal.getId();
+    public String getTerminalId() {
+        return _terminal.getId();
     }
 }
